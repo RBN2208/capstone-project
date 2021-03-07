f42ab18efab58cd3c3868108664df3f214607548
Robin Steinhauser <rbn2208@gmail.com>
2021-03-07 19:43:02 +0100

    Updatet Setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <AppContainer></AppContainer>
 }
 
-export default App;
+const AppContainer = styled.div`
+  display: grid;
+`
