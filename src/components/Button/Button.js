98139125e5142d4100f7a21f9d0aa1d974e35bed
Robin Steinhauser <rbn2208@gmail.com>
2021-03-10 11:27:38 +0100

    Changed button padding to em, so hopefully it will become a bit responsive

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components/macro'
 
 export default styled.button`
+  -webkit-appearance: none;
+  -moz-appearance: none;
   appearance: none;
   background-color: ${props => props.bgColor.name};
   border: none;
   color: white;
-  padding: 1px 8px;
+  padding: 0.2em 0.6em;
   border-radius: 3px;
   font-size: 1.2rem;
 `
