225312487142fc53817ba0ffcfd1b453b59bf985
Robin Steinhauser <rbn2208@gmail.com>
2021-03-15 17:01:59 +0100

    Readded prettier, create BurgerMenu Component

---
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -0,0 +1,10 @@
+import { slide as Menu } from 'react-burger-menu'
+
+export default function BurgerMenu() {
+  return (
+    <Menu>
+      <a href="/">Home</a>
+      <a href="/history">History</a>
+    </Menu>
+  )
+}
