9524f7814f4a47dbbb8fea0a4434daa0b1b7b2f4
Robin Steinhauser <rbn2208@gmail.com>
2021-03-16 10:24:12 +0100

    Added animation for slide menu

--- src/components/SlideMenu/SlideMenu.js
+++ src/components/SlideMenu/SlideMenu.js
@@ -1,13 +1,16 @@
 import styled, { keyframes } from 'styled-components/macro'
 import Icon from 'supercons'
-import Button from '../Button/Button'
 
 export default function SlideMenu({ menuIsOpen, setMenuIsOpen }) {
   return (
     <MenuBox position={menuIsOpen}>
-      <p>Home</p>
-      <p>History</p>
-      <Icon glyph="view-close-small" onClick={() => setMenuIsOpen(false)} />
+      <Nav>
+        <span>Home</span>
+        <span>History</span>
+      </Nav>
+      <IconBox>
+        <Icon glyph="view-close-small" onClick={() => setMenuIsOpen(false)} />
+      </IconBox>
     </MenuBox>
   )
 }
@@ -25,12 +28,22 @@ const slideOut = keyframes`
 const MenuBox = styled.div`
   position: fixed;
   left: ${props => (props.position ? '0' : '-100vw')};
-  background-color: cornflowerblue;
+  display: ${props => (props.position ? 'inline' : 'none')};
+  background-color: gainsboro;
   width: 50vw;
   height: 100vh;
-  padding: 20px;
+  padding: 50px 20px;
   z-index: 1;
   animation-name: ${props => (props.position ? slideIn : slideOut)};
   animation-duration: 1s;
   animation-iteration-count: 1;
+`
+const Nav = styled.nav`
+  display: grid;
+  gap: 30px;
+`
+const IconBox = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `
