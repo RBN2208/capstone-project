de9ca54884c20322914d195983d80434bb199a48
Robin Steinhauser <rbn2208@gmail.com>
2021-03-25 14:18:37 +0100

    Test for mobile

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -4,7 +4,8 @@ export default function Images({ url }) {
   return <IMG src={url} alt="" />
 }
 const IMG = styled.img`
-  max-width: 300px;
-  max-height: auto;
+  max-width: 100%;
+  min-width: 300px;
+  height: auto;
   scroll-snap-align: start;
 `
