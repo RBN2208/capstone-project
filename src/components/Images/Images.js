b1fe14aa5176a8d2c5c8e70376860c1fe8cdb1ae
Robin Steinhauser <rbn2208@gmail.com>
2021-03-25 14:24:51 +0100

    Next

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -4,8 +4,8 @@ export default function Images({ url }) {
   return <IMG src={url} alt="" />
 }
 const IMG = styled.img`
-  max-width: 100%;
-  min-width: 300px;
+  max-width: 100% !important;
+  min-width: 300px !important;
   height: auto;
   scroll-snap-align: start;
 `
