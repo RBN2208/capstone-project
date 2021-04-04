88bc00fb591939d861403cdccc04e8be30149a40
Robin Steinhauser <rbn2208@gmail.com>
2021-04-04 12:12:48 +0200

    updates

--- src/components/FormComponents/NewService.jsx
+++ src/components/FormComponents/NewService.jsx
@@ -58,7 +58,9 @@ export default function NewService({ onAddNewService, onAbort }) {
   function handleSubmit(event) {
     event.preventDefault()
     const formElements = event.target.elements
-    const costsPerHour = formElements.costs.value.replace(/[^\d]/g, '')
+    const toRound = parseFloat(formElements.setcosts.value)
+    const costsPerHour = Math.round(toRound / 0.5) * 0.5
+
     const data = {
       name: formElements.service.value,
       costs: Number(costsPerHour) || Number(50),
